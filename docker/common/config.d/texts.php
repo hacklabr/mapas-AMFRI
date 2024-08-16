5eb9bba87c3befd31eed109bb767a210aa390bd5
Welington De Lima Olegario <wolegario49@gmail.com>
2024-08-16 17:43:36 -0300

    Faz a criação da configuração dos texto no ambiente de produção

---
+++ docker/common/config.d/texts.php
@@ -0,0 +1,17 @@
+<?php
+
+use \MapasCulturais\i;
+
+return [
+    "text:home-header.title" => i::__("Bem-vind@!"),
+    "text:home-header.description" => i::__("O Mapa Cultural CIM-AMFRI / SC é uma plataforma livre, colaborativa e interativa de mapeamento do cenário cultural dos municípios consorciados associados ao Programa de Gestão Associada de Serviços Públicos na Área da Cultura na Região da AMFRI. Ele é um instrumento de governança digital que busca aprimorar a gestão pública, os mecanismos de participação e a democratização do acesso às políticas culturais promovidas nesta região. Além disso, é por meio desta plataforma que estamos garantindo a estruturação do Sistemas de Informações e Indicadores culturais dos municípios de Balneário Piçarras, Bombinhas, Camboriú, Ilhota, Itajaí, Itapema, Navegantes, Penha e Porto Belo."),
+    "text:home-entities.events" => i::__("Quer saber quais eventos culturais estão ocorrendo na região da AMFRI? Utilize as ferramentas de busca para encontrar o que está rolando. Como usuário cadastrado, você também pode incluir seus eventos na plataforma e divulgá-los gratuitamente!"),
+    "text:home-entities.agents" => i::__("Encontre artistas, gestores e produtores registrados na plataforma, formando uma rede de agentes envolvidos na cena cultural da região. Cadastre-se e inclua também seus coletivos, bandas, instituições e empresas das quais faz parte."),
+    "text:home-entities.opportunities" => i::__("Aqui você pode realizar inscrições e acessar o resultado de editais e oportunidades como chamamentos públicos, oficinas, prêmios, concursos etc. Além disso, é possível criar seu próprio formulário e divulgar suas oportunidades para outros agentes culturais da região."),
+    "text:home-entities.projects" => i::__("Este é o espaço onde você pode acessar chamamentos públicos, leis de fomento, mostras, convocatórias e editais criados, além de diversas iniciativas cadastradas por usuários da plataforma. Explore e conecte-se com projetos inovadores na região."),
+    "text:home-entities.spaces" => i::__("Procure por espaços culturais disponíveis na plataforma usando campos de busca combinada que aumentam a precisão de sua pesquisa. Cadastre os locais onde desenvolve suas atividades artísticas e culturais para facilitar a busca de outros usuários."),
+    "text:home-feature.description" => i::__("Confira os últimos destaques de cada uma das áreas."),
+    "text:home-register.description" => i::__("Faça seu cadastro e colabore com o Mapa Cultural CIM-AMFRI / SC. Participe desta plataforma livre, colaborativa e interativa de mapeamento do cenário cultural e instrumento de governança digital, contribuindo para o aprimoramento da gestão pública, dos mecanismos de participação e da democratização do acesso às políticas culturais promovidas na região."),
+    'text:home-developers.description' => i::__("O Mapa Cultural CIM-AMFRI / SC é um software livre, criado em parceria entre hacklab/, secretarias de cultura, organizações não governamentais, empresas e coletivos que investem na plataforma. Você pode contribuir para seu desenvolvimento através do GitHub."),
+    "text:home-map.description" => i::__("Os agentes, os espaços e os eventos cadastrados contam com a geolocalização de seus endereços, encontre-os aqui:"),
+];
